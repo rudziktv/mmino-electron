061e79c14c791fd2cf43b795efb840b835626a85
rudziktv <auto75208@gmail.com>
2023-11-07 09:00:55 +0100

    xd

--- src/components/LastPlayedItem/LastPlayedItem.tsx
+++ src/components/LastPlayedItem/LastPlayedItem.tsx
@@ -1,23 +1,54 @@
+import { useRef } from "react";
 import FilledIconButton from "../../design/interface/Button/IconButtons/FilledIconButton";
-import TonalIconButton from "../../design/interface/Button/IconButtons/TonalIconButton";
+import useRipple from "../../hooks/useRipple";
 import "./LastPlayedItem.css";
 
 const LastPlayedItem = (props: LastPlayedItemProps) => {
+    const ref = useRef(null);
+    const [ripples, invokeRipple] = useRipple(ref);
+
     return (
-        <div className={`last-played-item ${props.className}`}>
-            <div className="last-played-item-cover">
+        <div
+            className={`last-played-item ${props.className}`}
+            ref={ref}
+            onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                invokeRipple(e);
+                props.onClick?.();
+            }}
+        >
+            {ripples}
+            <div
+                className="last-played-item-cover"
+                style={{
+                    backgroundImage: `url(${props.thumbnail})`,
+                }}
+            >
                 <div className="last-played-item-cover-play">
                     <FilledIconButton icon="ri-play-fill" />
                 </div>
-                <i className="ri-music-line last-played-item-cover-icon" />
+                {!props.thumbnail && (
+                    <i className="ri-music-line last-played-item-cover-icon" />
+                )}
             </div>
-            <span>Title</span>
+            <div className="last-played-item-info">
+                <span className="last-played-item-title">Title</span>
+                <span className="last-played-item-artist">Type ○ Artist</span>
+            </div>
         </div>
     );
 };
 
 interface LastPlayedItemProps {
     className?: string;
+
+    id?: string;
+    title?: string;
+    artist?: string;
+    thumbnail?: string;
+    onClick?: () => void;
+    type?: "Album" | "Playlist" | "Track" | "Favourites" | "Undefined";
 }
 
 export default LastPlayedItem;
